9095140fa2673067d97dc800a473c369f8ca9d6f
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-23 22:11:27 +0900

    add Home and Admin components

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Movies from './components/Movies';
+import Admin from './components/Admin';
+import Home from './components/Home';
 
 export default function App() {
   return (
@@ -45,12 +47,4 @@ export default function App() {
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>HOME</h2>;
-}
-
-function Admin() {
-  return <h2>Manage Catelogue</h2>;
 }
