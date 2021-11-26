5664430b769027824dfb71e36489532fd7dbf0be
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-26 21:39:54 +0900

    add alert

--- go-movies/src/components/EditMovie.js
+++ go-movies/src/components/EditMovie.js
@@ -3,6 +3,7 @@ import './EditMovie.css';
 import Input from './form-components/Input';
 import Select from './form-components/Select';
 import TextArea from './form-components/Textarea';
+import Alert from './ui-components/Alert';
 
 export default class EditMovie extends Component {
   constructor(props) {
@@ -27,6 +28,10 @@ export default class EditMovie extends Component {
       isLoaded: false,
       error: null,
       errors: [],
+      alert: {
+        type: 'd-none',
+        message: '',
+      },
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -60,7 +65,15 @@ export default class EditMovie extends Component {
     fetch('http://localhost:8081/v1/admin/editmovie', requestOptions)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        if (data.error) {
+          this.setState({
+            alert: { type: 'alert-danger', message: data.error.message },
+          });
+        } else {
+          this.setState({
+            alert: { type: 'alert-success', message: 'Changes saved!' },
+          });
+        }
       });
   };
 
@@ -130,6 +143,10 @@ export default class EditMovie extends Component {
       return (
         <Fragment>
           <h2>Add/Edit Movie</h2>
+          <Alert
+            alertType={this.state.alert.type}
+            alertMessage={(this, this.state.alert.message)}
+          />
           <hr />
           <form onSubmit={this.handleSubmit}>
             <input
