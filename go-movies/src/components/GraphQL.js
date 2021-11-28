d488731d66af9bfd2b01053ad8f02ff6b50cd2d9
nao-18 <kyahyo17@yahoo.co.jp>
2021-11-28 17:08:56 +0900

    fixed the movie search function

--- go-movies/src/components/GraphQL.js
+++ go-movies/src/components/GraphQL.js
@@ -25,7 +25,13 @@ export default class GraphQL extends Component {
       searchTerm: value,
     }));
 
-    this.performSearch();
+    if (value.length > 2) {
+      this.performSearch();
+    } else {
+      this.setState({
+        movies: [],
+      });
+    }
   };
 
   performSearch() {
